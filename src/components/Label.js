5ff089d7abeb94a8ce91388ce0961593eadcdc4b
Demian Dekoninck <demian@live.be>
2020-03-04 17:39:10 +0100

    make form labels bold

--- src/components/Label.js
+++ src/components/Label.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Label = ({ children, ...props }) => {
   return (
-    <label className="block mb-2 cursor-pointer" {...props}>
+    <label className="block mb-2 cursor-pointer font-bold" {...props}>
       {children}
     </label>
   );
