d76f67000183ebe912e6e40b9195cd27c36bb43a
Demian Dekoninck <demian@live.be>
2020-05-04 13:02:40 +0200

    wip

--- ssn-server/packages/ssn-example-parkings-gent/src/snippets/createSnippets.js
+++ ssn-server/packages/ssn-example-parkings-gent/src/snippets/createSnippets.js
@@ -1,90 +1,107 @@
 import fs from 'fs';
 import N3 from 'n3';
+import path from 'path';
 import { promisify } from 'util';
 import { v4 as uuidv4 } from 'uuid';
-import { SOSA, RDF } from '../vocs.js';
 import { exec } from '../utils.js';
+import { MP, RDF, HYDRA, PROV, XSD } from '../vocs.js';
+import stringifyQuads from '../../../ssn-core/src/utils/stringifyQuads.js';
 
 const writeFile = promisify(fs.writeFile);
+const appendFile = promisify(fs.appendFile);
 const stat = promisify(fs.stat);
 
-const mapStoreToObservations = store => {
-  const subjects = store.getSubjects(RDF('type'), SOSA('Observation'));
+const { quad, namedNode, literal } = N3.DataFactory;
 
-  return subjects.map(subject => {
-    const result = store.getObjects(subject, SOSA('hasSimpleResult'))[0];
-    const resultTime = store.getObjects(subject, SOSA('resultTime'))[0];
+class CreateSnippets {
+  constructor(tree, collection, snippetSizes, dataPath, nodesPath) {
+    this.tree = tree;
+    this.collection = collection;
+    this.snippetSizes = snippetSizes;
+    this.dataPath = dataPath;
+    this.nodesPath = nodesPath;
 
-    return { date: resultTime.value, value: result.value };
-  });
-};
+    this.queue = {};
+    this.currentProcess = undefined;
+  }
 
-const getData = async (dataPath, pages) => {
-  return Promise.all(
-    pages.map(page => {
-      return new Promise((resolve, reject) => {
-        const store = new N3.Store();
-        const parser = new N3.Parser();
+  create(node) {
+    this.queue[node.nodeNumber] = node;
 
-        const rdfStream = fs.createReadStream(`${dataPath}/${page}.ttl`);
+    if (this.currentProcess === undefined) {
+      this._handleNext();
+    }
+  }
 
-        parser.parse(rdfStream, (err, quad) => {
-          if (err) {
-            reject(err);
-          } else if (quad) {
-            store.addQuad(quad);
-          } else {
-            resolve(mapStoreToObservations(store));
-          }
-        });
+  async _process(node) {
+    const { mtime: mtimeBefore } = await stat(`${this.nodesPath}/${node.nodeNumber}`);
+
+    const pages = this.tree.getLeavesForNode(node).map(page => {
+      return path.resolve(this.dataPath, `${page}.ttl`);
+    });
+
+    const tempFile = `./temp/${uuidv4()}.json`;
+
+    await writeFile(tempFile, JSON.stringify({ snippet_sizes: this.snippetSizes, pages: pages }));
+
+    const results = await exec(
+      `python3 ./src/snippets/snippets.py ${tempFile} ${this.snippetSizes.join(',')}`
+    );
+
+    fs.unlink(tempFile, () => {});
+    delete this.queue[node.nodeNumber];
+
+    const { mtime: mtimeAfter } = await stat(`${this.nodesPath}/${node.nodeNumber}`);
+
+    if (mtimeAfter.getTime() === mtimeBefore.getTime()) {
+      // File has not changed
+      const quads = this._quads(node, JSON.parse(results));
+
+      // TODO: don't import stringifyQuads from core library, maybe we need to expose a @ssn/utils package?
+      appendFile(`${this.nodesPath}/${node.nodeNumber}`, stringifyQuads(quads));
+    }
+
+    this._handleNext();
+  }
+
+  _quads(node, results) {
+    const quads = [];
+    // TODO: this should not be defined here because this is dependent of the value specified in the (LD)Disk
+    const nodeSubject = `${this.collection.getSubject().id}/node/${node.nodeNumber}`;
+
+    Object.entries(results).map(([snippet_size, snippets]) => {
+      const subject = namedNode(`${nodeSubject}/snippets/${snippet_size}`);
+
+      quads.push(
+        quad(subject, RDF('type'), HYDRA('Collection')),
+        quad(subject, PROV('wasDerivedFrom'), namedNode(nodeSubject))
+      );
+
+      snippets.map(({ date, fraction }) => {
+        const snippetSubject = namedNode(`${subject.value}/${date}`);
+
+        quads.push(
+          quad(subject, HYDRA('member'), snippetSubject),
+          quad(snippetSubject, RDF('type'), MP('Snippet')),
+          quad(snippetSubject, MP('snippet_size'), literal(snippet_size)),
+          quad(snippetSubject, MP('fraction'), literal(fraction)),
+          quad(snippetSubject, MP('index'), literal(date, XSD('dateTime')))
+        );
       });
-    })
-  ).then(results => results.flat());
-};
+    });
 
-const createSnippets = async (tree, node, snippetSizes, dataPath, nodesPath) => {
-  const { mtime: mtimeBefore } = await stat(`${nodesPath}/${node.nodeNumber}`);
-
-  const pages = tree.getLeavesForNode(node);
-
-  const firstPageDate = new Date(pages[0]).getTime();
-  const lastPageDate = new Date(pages[pages.length - 1]).getTime();
-
-  const differenceInMilliseconds = lastPageDate - firstPageDate;
-  const differenceInDays = Math.floor(differenceInMilliseconds / (1000 * 60 * 60 * 24));
-
-  const filteredSnippetSizes = snippetSizes;
-  // const filteredSnippetSizes = snippetSizes.filter(
-  //   snippetSize => differenceInDays > snippetSize * 2
-  // );
-
-  if (filteredSnippetSizes.length <= 0) {
-    // return;
+    return quads;
   }
 
-  const data = await getData(dataPath, pages);
-  const tempFile = `./temp/${uuidv4()}.json`;
+  _handleNext() {
+    const nextNodes = Object.values(this.queue);
 
-  await writeFile(tempFile, JSON.stringify(data));
-
-  const { mtime: mtimeAfter } = await stat(`${nodesPath}/${node.nodeNumber}`);
-
-  if (mtimeAfter.getTime() > mtimeBefore.getTime()) {
-    // File has changed
-    return;
+    if (nextNodes.length === 0) {
+      this.currentProcess = undefined;
+    } else {
+      this.currentProcess = this._process(nextNodes[0]);
+    }
   }
+}
 
-  const results = await Promise.all(
-    filteredSnippetSizes.map(snippetSize => {
-      return exec(`python3 ./src/snippets/snippets.py ${tempFile} ${snippetSize}`);
-    })
-  );
-
-  console.log(results);
-
-  // fs.unlink(tempFile, () => {});
-
-  // console.log({ firstPageDate, lastPageDate, differenceInDays, data: JSON.stringify(data) });
-};
-
-export default createSnippets;
+export default CreateSnippets;
--- ssn-server/packages/ssn-example-parkings-gent/src/snippets/snippets.py
+++ ssn-server/packages/ssn-example-parkings-gent/src/snippets/snippets.py
@@ -1,26 +1,61 @@
 import sys
+import os
 import json
+from rdflib import Graph, Namespace
 import matrixprofile as mp
 
-taxi = mp.datasets.load('nyc-taxi-anomalies')
-ts = taxi['data']
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
+blockPrint()
 
 dataPath = sys.argv[1]
-snippetsSize = int(sys.argv[2])
 
 file = open(sys.argv[1], 'r')
 content = json.loads(file.read())
 
+pages = content['pages']
+snippet_sizes = content['snippet_sizes']
+
 dates = []
 values = []
 
-for row in content:
-    dates.append(row['date'])
-    values.append(float(row['value']))
+RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+SOSA = Namespace("http://www.w3.org/ns/sosa/")
 
+for page in pages:
+    graph = Graph()
+    graph.load(page, format="trig")
 
-test = ts[:len(values)]
+    for s, p, o in graph.triples((None, RDF.type, SOSA.Observation)):
+        _, _, value = next(graph.triples((s, SOSA.hasSimpleResult, None)))
+        _, _, date = next(graph.triples((s, SOSA.resultTime, None)))
 
-snippets = mp.discover.snippets(values, snippetsSize, 4)
+        values.append(value)
+        dates.append(date)
 
-print(snippets)
+result = {}
+
+for snippet_size in snippet_sizes:
+    jsonObjects = []
+    snippets = mp.discover.snippets(values, snippet_size, 4)
+
+    for snippet in snippets:
+        date = dates[snippet['index']]
+        fraction = snippet['fraction']
+
+        formattedDate = date.value.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+
+        jsonObjects.append({
+            "date": formattedDate, 
+            "fraction": fraction 
+        })
+
+    result[snippet_size] = jsonObjects
+
+enablePrint()
+
+print(json.dumps(result))
