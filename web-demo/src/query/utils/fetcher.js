049e97732755c601b4ac99036bb3a0c0d304f7d8
Demian Dekoninck <demian@live.be>
2020-05-09 11:10:23 +0200

    Moving web-demo to a subdirectory

--- web-demo/src/query/utils/fetcher.js
+++ web-demo/src/query/utils/fetcher.js
@@ -3,15 +3,15 @@ import ldfetch from 'ldfetch';
 const fetcher = new ldfetch();
 
 fetcher.on('cache-miss', () => {
-  console.log('cache-miss');
+  //console.log('cache-miss');
 });
 
 fetcher.on('cache-hit', () => {
-  console.log('cache-hit');
+  //console.log('cache-hit');
 });
 
 fetcher.on('downloaded', () => {
-  console.log('cache-downloaded');
+  //console.log('cache-downloaded');
 });
 
 export default fetcher;
